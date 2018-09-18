5845a839954b2439c25ce5b64e00cb899d7eb34f
Karan Navani <kknavani@gmail.com>
2018-09-18 17:36:31 +0100

    added public to index.js

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const morgan              = require('morgan');
 const errorHandler        = require('./lib/errorHandler');
 const Router              = require('./config/routes');
 const app                 = express();
-const mongoose            = require('mongoose');
 const { dbUri, port }     = require('./config/environment');
+const mongoose            = require('mongoose');
 mongoose.Promise          = require('bluebird');
 
 mongoose.connect(dbUri);
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api', Router);
 
